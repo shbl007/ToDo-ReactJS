fbcf355aef08fb083fbfe7c678c6ed7b149c4efb
shibil <shbl.salim@gmail.com>
2024-01-18 11:54:41 +0530

    Initial commit: Set up basic structure and functionality for ToDo app using ReactJS.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+function App() { 
+  const[toDos,setToDos]=useState([])
+  const[toDo,setToDo]=useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+         <h2>Enter Quick Task here!! </h2>
+      </div>
+      <div className="input">
+        <input
+        value={toDo}
+         onChange={(e)=>setToDo(e.target.value)} type="text"
+         placeholder="🖊️ Add item..." />
+        <i onClick={()=>setToDos([...toDos,{id:Date.now(), text:toDo,status:false}])}
+         className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {
+          toDos.map((obj) => {
+return(
+        <div className="todo">
+          <div className="left">
+            <input
+            onChange={(e)=>{
+                console.log(e.target.value)
+                console.log(obj)
+                // obj.status=!obj.status;
+                setToDos(toDos.filter(obj2=>{
+                  if(obj2.id===obj.id){
+                  obj2.status=e.target.value
+                  }
+                  return obj2
+                }))
+            }}
+            
+            value={obj.status}
+            type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
+          </div>
+          <div className="right">
+            <i className="fas fa-times"></i>
+          </div>
+        </div>
+) 
+          })}
+      </div>
     </div>
   );
 }

16df108d9aec85c6a19248155dd89f410aaf6e22
shibil <shbl.salim@gmail.com>
2024-01-19 18:08:45 +0530

    Implement todo item deletion on click

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() { 
-  const[toDos,setToDos]=useState([])
-  const[toDo,setToDo]=useState('')
+function App() {
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState('');
+
+  const handleDelete = (todoId) => {
+    setToDos(toDos.filter((todo) => todo.id !== todoId));
+  };
+
   return (
     <div className="app">
       <div className="mainHeading">
@@ -11,45 +16,54 @@ function App() {
       </div>
       <div className="subHeading">
         <br />
-         <h2>Enter Quick Task here!! </h2>
+        <h2>Enter Quick Task here!! </h2>
       </div>
       <div className="input">
         <input
-        value={toDo}
-         onChange={(e)=>setToDo(e.target.value)} type="text"
-         placeholder="🖊️ Add item..." />
-        <i onClick={()=>setToDos([...toDos,{id:Date.now(), text:toDo,status:false}])}
-         className="fas fa-plus"></i>
+          value={toDo}
+          onChange={(e) => setToDo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i
+          onClick={() =>
+            setToDos([
+              ...toDos,
+              { id: Date.now(), text: toDo, status: false },
+            ])
+          }
+          className="fas fa-plus"
+        ></i>
       </div>
       <div className="todos">
-        {
-          toDos.map((obj) => {
-return(
-        <div className="todo">
-          <div className="left">
-            <input
-            onChange={(e)=>{
-                console.log(e.target.value)
-                console.log(obj)
-                // obj.status=!obj.status;
-                setToDos(toDos.filter(obj2=>{
-                  if(obj2.id===obj.id){
-                  obj2.status=e.target.value
-                  }
-                  return obj2
-                }))
-            }}
-            
-            value={obj.status}
-            type="checkbox" name="" id="" />
-            <p>{obj.text}</p>
+        {toDos.map((obj) => (
+          <div className="todo" key={obj.id}>
+            <div className="left">
+              <input
+                onChange={(e) => {
+                  console.log(e.target.value);
+                  console.log(obj);
+                  setToDos(
+                    toDos.map((obj2) => {
+                      if (obj2.id === obj.id) {
+                        obj2.status = e.target.value;
+                      }
+                      return obj2;
+                    })
+                  );
+                }}
+                value={obj.status}
+                type="checkbox"
+                name=""
+                id=""
+              />
+              <p>{obj.text}</p>
+            </div>
+            <div className="right">
+              <i onClick={() => handleDelete(obj.id)} className="fas fa-times"></i>
+            </div>
           </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
-) 
-          })}
+        ))}
       </div>
     </div>
   );
